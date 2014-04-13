056d871dc32f1f6e8305e746325ea48b43023bed
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-13 15:55:00 +0600

    [fix] Visual fix updating dict.

--- GIS/cafe/serializers.py
+++ GIS/cafe/serializers.py
@@ -22,9 +22,6 @@ class AddressSerializer(ModelSerializer):
 
     def to_native(self, obj):
         result = super(AddressSerializer, self).to_native(obj)
-        result['cuisines'] = result['cafe']['cuisines']
-        result['types'] = result['cafe']['types']
-        result['phones'] = result['cafe']['phones']
-        result['min_price'] = result['cafe']['min_price']
-        result['cafe'] = result['cafe']['name']
+        result.update(result['cafe'])
+        result.pop('cafe')
         return result
