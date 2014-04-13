58a4ae26dd2a894ea5abb46b7ef9a107bb54cee5
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-13 17:36:52 +0600

    [fix] 400 for wrong query params.

--- GIS/cafe/views.py
+++ GIS/cafe/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 from GIS.cafe.filters import AddressFilterBackend
@@ -10,3 +12,10 @@ class AddressesViewSet(mixins.ListModelMixin, GenericViewSet):
     serializer_class = AddressSerializer
     model = Address
     filter_backend = AddressFilterBackend
+
+    def list(self, request, *args, **kwargs):
+        if ('distance' in request.QUERY_PARAMS and
+                (not 'longitude' in request.QUERY_PARAMS or not 'latitude' in request.QUERY_PARAMS)):
+            return Response('Specify your position (longitude & latitude) if you specify distance.',
+                            status=HTTP_400_BAD_REQUEST)
+        return super(AddressesViewSet, self).list(request, *args, **kwargs)
