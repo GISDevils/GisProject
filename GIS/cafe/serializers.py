313f702c2652ff207a30bf12f79f3322a754af31
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-13 15:09:33 +0600

    [fix] Unzip cafe dict in response.

--- GIS/cafe/serializers.py
+++ GIS/cafe/serializers.py
@@ -20,3 +20,11 @@ class AddressSerializer(ModelSerializer):
 
     cafe = CafeSerializer()
 
+    def to_native(self, obj):
+        result = super(AddressSerializer, self).to_native(obj)
+        result['cuisines'] = result['cafe']['cuisines']
+        result['types'] = result['cafe']['types']
+        result['phones'] = result['cafe']['phones']
+        result['min_price'] = result['cafe']['min_price']
+        result['cafe'] = result['cafe']['name']
+        return result
