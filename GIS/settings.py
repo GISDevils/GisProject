4f831c535f50d92117d30a4037fe2b32a8e0787c
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-15 06:19:06 +0600

    [fix] Included cafe in INSTALLED_APPS.

--- GIS/settings.py
+++ GIS/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'rest_framework',
+    'GIS.cafe',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
