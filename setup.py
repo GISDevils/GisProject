8041061ea079310c7ae596e3704e2f2ee86e82ce
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-15 07:17:25 +0600

    [fix] Openshift setup info.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
-    version='1.0',
+    name='GisProject',
+    version='0.1',
     description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
+    author='Dmitry Yantsen, Liza Lukicheva, Evgeny Muralev, Elena Kozhevina',
+    author_email='d.yantse@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django==1.5', 'geopy', 'djangorestframework==2.3.12'],
+    install_requires=['Django==1.5', 'geopy', 'djangorestframework==2.3.12', 'mysql-python'],
 )
