663e4758ae57720201315f52e3a86d7e7936314d
Dmitry Yantsen <d.yantsen@gmail.com>
2014-04-17 03:16:55 +0600

    [fix] Image serving & relative urls.

--- GIS/urls.py
+++ GIS/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
+from GIS import settings
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^cafe/', include('GIS.cafe.urls')),
-    url(r'^$', 'GIS.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
